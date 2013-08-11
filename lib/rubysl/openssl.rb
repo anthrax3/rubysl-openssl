70eefe8201b73cfb97cdf6898a6cfb6e67bbda71
Brian Shirai <brixen@gmail.com>
2013-08-11 16:19:12 -0700

    Fixed C-ext location for gem install.

--- lib/rubysl/openssl.rb
+++ lib/rubysl/openssl.rb
@@ -1,2 +1,25 @@
+=begin
+= $RCSfile$ -- Loader for all OpenSSL C-space and Ruby-space definitions
+
+= Info
+  'OpenSSL for Ruby 2' project
+  Copyright (C) 2002  Michal Rokos <m.rokos@sh.cvut.cz>
+  All rights reserved.
+
+= Licence
+  This program is licenced under the same licence as Ruby.
+  (See the file 'LICENCE'.)
+
+= Version
+  $Id$
+=end
+
+require 'openssl/openssl'
+require 'openssl/bn'
+require 'openssl/cipher'
+require 'openssl/config'
+require 'openssl/digest'
+require 'openssl/x509'
+require 'openssl/ssl'
+
 require "rubysl/openssl/version"
-require "rubysl/openssl/openssl"
