eadfd3285cc7a9fc7da8bf64a46676c2e2e1818c
Brian Shirai <brixen@gmail.com>
2013-08-27 15:53:38 -0700

    Imported files from 1.8.

--- lib/rubysl/openssl.rb
+++ lib/rubysl/openssl.rb
@@ -1,2 +1,28 @@
-require "rubysl/openssl/version"
-require "rubysl/openssl/openssl"
+=begin
+= $RCSfile$ -- Loader for all OpenSSL C-space and Ruby-space definitions
+
+= Info
+  'OpenSSL for Ruby 2' project
+  Copyright (C) 2002  Michal Rokos <m.rokos@sh.cvut.cz>
+  All rights reserved.
+
+= Licence
+  This program is licenced under the same licence as Ruby.
+  (See the file 'LICENCE'.)
+
+= Version
+  $Id: openssl.rb 29856 2010-11-22 07:21:45Z shyouhei $
+=end
+
+require 'thread'
+require 'openssl/openssl'
+
+require 'openssl/bn'
+require 'openssl/cipher'
+require 'openssl/config'
+require 'openssl/digest'
+require 'openssl/pkcs7'
+require 'openssl/ssl-internal'
+require 'openssl/x509-internal'
+
+require 'rubysl/openssl/version'
