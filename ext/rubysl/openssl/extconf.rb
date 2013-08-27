b13af7b04cedf99d9d9fd0f8a2bf5159c0ebe677
Brian Shirai <brixen@gmail.com>
2013-08-27 16:22:36 -0700

    Fixed scoping of ext.

--- ext/rubysl/openssl/extconf.rb
+++ ext/rubysl/openssl/extconf.rb
@@ -140,5 +140,5 @@ have_struct_member("X509_ATTRIBUTE", "single", "openssl/x509.h")
 message "=== Checking done. ===\n"
 
 create_header
-create_makefile("openssl")
+create_makefile("openssl/openssl")
 message "Done.\n"
