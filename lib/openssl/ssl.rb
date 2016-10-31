343b1cbcb65920b4c23aa4cdc90d93f86681bbc5
Brian Shirai <brixen@gmail.com>
2016-10-31 11:20:10 +0900

    Updated to ruby:ded40392317c885439232de5f7da3519ebce4ee5.

--- lib/openssl/ssl.rb
+++ lib/openssl/ssl.rb
@@ -251,7 +251,7 @@ module OpenSSL
       include SocketForwarder
 
       if ExtConfig::OPENSSL_NO_SOCK
-        def initialize(io, ctx = nil); raise NotImplmentedError; end
+        def initialize(io, ctx = nil); raise NotImplementedError; end
       else
         if ExtConfig::HAVE_TLSEXT_HOST_NAME
           attr_accessor :hostname
@@ -290,7 +290,10 @@ module OpenSSL
       # call-seq:
       #    ssl.sysclose => nil
       #
-      # Shuts down the SSL connection and prepares it for another connection.
+      # Sends "close notify" to the peer and tries to shut down the SSL
+      # connection gracefully.
+      #
+      # If sync_close is set to +true+, the underlying IO is also closed.
       def sysclose
         return if closed?
         stop
